47bc7921afb453fd1a0bba85825a310b33374916
f.landheer <frank.landheer@cofano.nl>
2021-04-08 11:34:29 +0700

    Improvements Sylvia

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def sylvanator(csv_file):
     '''
 
     df = pd.read_csv(csv_file)
-    df['dayofweek'] = list(df['date'].map(str) + " " + pd.to_datetime(df['date'], format='%Y-%m-%d').dt.day_name() )
-    new = list(df['description'].map(str)+ " "+ df['name.1'].map(str) + " "+ df['weight'].map(str)+ " "+ df['name.2'].map(str))
-    df['new'] = new
-    df = df.drop_duplicates(subset=["name", "dayofweek"])
-    sylvanator = df.pivot(index = 'name', columns = 'dayofweek')['new']
+    df['dayofweek'] = list(df['loadDate'].map(str) + " " + pd.to_datetime(df['loadDate'], format='%Y-%m-%d').dt.day_name() )
+    new = list(df['loadlocation'].map(str) + " > "+ df['dischargeLocation'].map(str) + " " + df['product'].map(str) + " "+ df['weight'].map(str)+ "t" + " "+ df['volume'].map(str)+ "v"+  " "+ df['supplier'].map(str))
+    df["new"] = new
+    df = df.drop_duplicates(subset=["loadlocation", "dayofweek"])
+    sylvanator = df.pivot(index = 'loadlocation', columns = 'dayofweek')['new']
 
     print(sylvanator.head())
 
