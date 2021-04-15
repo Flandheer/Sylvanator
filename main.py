bfcf2727069fdbc08ffd21b081c6551c8d6a3542
f.landheer <frank.landheer@cofano.nl>
2021-04-15 23:20:28 +0700

    Volgorde tekst veranderd.

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def sylvanator(csv_file):
     '''
 
     df = pd.read_csv(csv_file)
-    df['dayofweek'] = list(df['loadDate'].map(str) + " " + pd.to_datetime(df['loadDate'], format='%Y-%m-%d').dt.day_name() )
-    new = list(df['loadlocation'].map(str) + " > "+ df['dischargeLocation'].map(str) + " " + df['product'].map(str) + " "+ df['weight'].map(str)+ "t" + " "+ df['volume'].map(str)+ "v"+  " "+ df['supplier'].map(str))
+    df['loadDate'] = df['loadDate'].str[:10]
+    df['dayofweek'] = list(
+    df['loadDate'].map(str) + " " + pd.to_datetime(df['loadDate'], format='%Y-%m-%d').dt.day_name())
+    new = list(df['loadlocation'].map(str) + " " + df['weight'].map(str) + "mt" + " " + df['volume'].map(str) + "m3" + " " + df['product'].map(str) + "-" + df['dischargeLocation'].map(str) + "(" + df['supplier'].map(str) + ")")
     df["new"] = new
-    df = df.drop_duplicates(subset=["loadlocation", "dayofweek"])
+    df = df.drop_duplicates(subset=["ship", "dayofweek"])
     sylvanator = df.pivot(index = 'ship', columns = 'dayofweek')['new']
 
     print(sylvanator.head())
