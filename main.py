ff71bcfcfc14232065e67e0f2be141e7e3f52817
f.landheer <frank.landheer@cofano.nl>
2021-06-22 07:43:56 +0200

    Added operators to table

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ def planning(df):
     df['loadDate'] = df['loadDate'].str[:10]
     df['dayofweek'] = list(
     df['loadDate'].map(str) + " " + pd.to_datetime(df['loadDate'], format='%Y-%m-%d').dt.day_name())
-    new = list(
-    df['loadlocation'].map(str) + " " + df['weight'].map(str) + "mt" + " " + df['volume'].map(str) + "m3" + " " +
-    df['product'].map(str) + "-" + df['dischargeLocation'].map(str) + "(" + df['supplier'].map(str) + ")")
+    new = list(df['loadlocation'].map(str) + " " + df['weight'].map(str) + "mt" +" " + df['volume'].map(str) +
+               "m3" + " " +df['product'].map(str) + "-" + df['dischargeLocation'].map(str) +
+               "(" + df['supplier'].map(str) + ")")
     df['new'] = new
+
+    barge = list(df['ship'].map(str) + " " + df['operator'].map(str))
+    df['barge'] = barge
+
     df = df.drop_duplicates(subset=["ship", "dayofweek"])
-    planning_result = df.pivot(index='ship', columns='dayofweek')['new']
+    planning_result = df.pivot(index='barge', columns='dayofweek')['new']
 
     return planning_result
 
 
 def status(df):
-    status = df[['ship', 'current_location', 'current_status', 'eta', 'ata']]
+    barge = list(df['ship'].map(str) + " " + df['operator'].map(str))
+    df['barge'] = barge
+    status = df[['barge', 'current_location', 'current_status', 'eta', 'ata']]
     status = status.drop_duplicates()
-    status['status'] = list(
-        status['current_location'].map(str) + " " + status['current_status'].map(str) + " ETA: " + status['eta'].map(
-            str) + " ATA: " + status['ata'].map(str))
-    status_result = status[['ship', 'status']]
+    status['status'] = list( status['current_location'].map(str) + " " + status['current_status'].map(str) + " ETA: "
+                             + status['eta'].map(str) + " ATA: " + status['ata'].map(str))
+    status_result = status[['barge', 'status']]
 
     return status_result
 
@@ -30,7 +35,7 @@ def sylvanator(csv_file):
     df = pd.read_csv(csv_file)
     plan = planning(df)
     state = status(df)
-    sylvanator = plan.merge(state, how='left', left_on='ship', right_on="ship")
+    sylvanator = plan.merge(state, how='left', left_on='barge', right_on="barge")
     sylvanator = sylvanator[
         [sylvanator.columns[0], sylvanator.columns[-1], sylvanator.columns[1], sylvanator.columns[2],
          sylvanator.columns[3], sylvanator.columns[4], sylvanator.columns[5]]]
